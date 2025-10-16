c4e762622e24acd52c2035a523caeed1bd52261e
Landin Schenkel <landinschenkel@gmail.com>
2025-10-15 23:40:42 -0400

    uptaded

--- wikipedia_api.py
+++ wikipedia_api.py
@@ -1,43 +1,45 @@
-import requests # Used to make HTTP requests to the Wikipedia API
+# Import requests to make HTTP requests to the Wikipedia API
+import requests
 
+# Define a function that fetches revision data for a given article
 def fetch_revisions(article_name):
-    # Define the Wikipedia API endpoint
+    # Wikipedia API endpoint
     url = "https://en.wikipedia.org/w/api.php"
-    
-    # Set up the parameters for the API request
+
+    # Define the parameters for the API request
     params = {
-        "action": "query", # We're querying data
-        "format": "json", # We want the response in JSON format
-        "prop": "revisions", # We want revision information
-        "titles": article_name, # The title of the article to fetch
-        "rvprop": "timestamp|user", # We want the timestamp and user of each
-        "rvlimit": "30", # Limit to the last 30 revisions
-        "redirects": "" # Follow redirects if the article name changes
+        "action": "query",              # We're querying data
+        "format": "json",               # We want the response in JSON format
+        "prop": "revisions",            # We want revision data
+        "titles": article_name,         # The article to look up
+        "rvprop": "timestamp|user",     # We want timestamp and username of each revision
+        "rvlimit": "30",                # Limit to 30 revisions
+        "redirects": ""                 # Follow redirects if the article name changes
     }
 
     try:
-        # Send thee GET request to Wikipedia
-        response = requests.get(url, params = params)
-        response.raise_for_status() # Raise an error if the request failed
+        # Send the GET request to Wikipedia
+        response = requests.get(url, params=params)
+        response.raise_for_status()  # Raise an error if the request failed
 
         # Parse the JSON response
         data = response.json()
 
         # Extract the 'query' section from the response
         query = data.get("query", {})
-        pages = query.get("pages", ())
+        pages = query.get("pages", {})
 
-        # If no pages were returned, the article dosn't exist
+        # If no pages were returned, the article doesn't exist
         if not pages:
             raise ValueError("No Wikipedia page found.")
-        
-        # Get the first page object ( there should only be one )
+
+        # Get the first page object (there should only be one)
         page = next(iter(pages.values()))
 
         # If the page is marked as missing, raise an error
         if "missing" in page:
             raise ValueError("Wikipedia page not found.")
-        
+
         # Get the list of revisions (timestamp and user)
         revisions = page.get("revisions", [])
 
@@ -53,7 +55,7 @@ def fetch_revisions(article_name):
             "redirected": redirected,
             "revisions": revisions
         }
-    
+
+    # Handle network-related errors
     except requests.exceptions.RequestException as e:
-        # Raise a connection error if the request failed
         raise ConnectionError("Unable to connect to Wikipedia.") from e
